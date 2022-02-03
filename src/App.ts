0ff5b0948edaa32a89edd37d352c98c0d673564c
matisantillan11 <matias1.santillan@gmail.com>
2022-02-02 22:30:35 -0300

    fixing port error

--- src/App.ts
+++ src/App.ts
@@ -41,7 +41,7 @@ export default class App implements Appeable {
 	public listen(): void {
 		let port: string = process.env.PORT;
 		if (port === undefined || port === null) {
-			port = '3030';
+			port = '3000';
 		}
 
 		this.app.use((req, res, next) => {
