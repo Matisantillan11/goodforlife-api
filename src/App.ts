3c75f35a0e2889fcc25c60ba2d5f434f634e3885
matisantillan11 <matias1.santillan@gmail.com>
2022-02-07 22:18:14 -0300

    port

--- src/App.ts
+++ src/App.ts
@@ -39,10 +39,10 @@ export default class App implements Appeable {
 	}
 
 	public listen(): void {
-		let port: string = process.env.PORT;
-		if (port === undefined || port === null) {
+		let port: string = process.env.PORT || '3001';
+		/* if (port === undefined || port === null) {
 			port = '3000';
-		}
+		} */
 
 		this.app.use((req, res, next) => {
 			res.header("Access-Control-Allow-Origin", "*");
